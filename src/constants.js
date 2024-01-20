951592279aae53b65635a8cd813fcd94315e0755
@AnnaKays <aakkiseleva@gmail.com>
2024-01-20 21:28:01 +0300

    Революция или эволюция? (часть 2)

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 export const COMMENTS_COUNT = 4;
-export const MOVIES_COUNT = 5;
+export const MOVIES_COUNT = 1;
 export const MOVIES_COUNT_TOP = 2;
+export const MOVIES_COUNT_ROW = 5;
+
--- src/presenter/films-presenter.js
+++ src/presenter/films-presenter.js
@@ -3,7 +3,7 @@ import FilmsView from '../view/films-view';
 import FilmsListView from '../view/films-list-view.js';
 import ShowMoreButtonView from '../view/show-more-button-view.js';
 import FilmCardView from '../view/film-card-view.js';
-import { MOVIES_COUNT, MOVIES_COUNT_TOP } from '../constants.js';
+import { MOVIES_COUNT, MOVIES_COUNT_TOP, MOVIES_COUNT_ROW } from '../constants.js';
 import FilmDetailsView from '../view/film-details-view.js';
 
 const footer = document.querySelector('.footer');
@@ -12,16 +12,25 @@ const body = document.querySelector('body');
 export default class FilmsPresenter {
   #mainContainer;
   #moviesModel;
-  #movies;
-
+  #movies; // массив с фильмами
+  #renderedMoviesCount = MOVIES_COUNT_ROW;
+  #allMoviesContainer;
+  #allMovies;
+  #allMoviesTitle;
   #main = new FilmsView(); // контейнер для списка фильмов .films
-  #allMovies = new FilmsListView({ name: 'All movies. Upcoming', hidden: true });
   #topRated = new FilmsListView({ name: 'Top rated' }, true);
+  #topRatedContainer = this.#topRated.element.querySelector('.films-list__container');
   #mostCommented = new FilmsListView({ name: 'Most commented' }, true);
+  #mostCommentedContainer = this.#mostCommented.element.querySelector('.films-list__container');
+  #showMoreButton = new ShowMoreButtonView();
 
   constructor(mainContainer, moviesModel) {
     this.#mainContainer = mainContainer;
     this.#moviesModel = moviesModel;
+    this.#movies = [...this.#moviesModel.movies]; // спред чтобы сделать новый массив и не сломать его
+    this.#allMoviesTitle = this.#movies.length > 0 ? 'All movies. Upcoming' : 'There are no movies in our database';
+    this.#allMovies = new FilmsListView({ name: this.#allMoviesTitle, hidden: this.#movies.length > 0 });
+    this.#allMoviesContainer = this.#allMovies.element.querySelector('.films-list__container');
   }
 
   #renderPopup = (film, popup) => {
@@ -59,33 +68,47 @@ export default class FilmsPresenter {
 
     link.addEventListener('click', (evt) => {
       evt.preventDefault();
-      openPopup(new FilmDetailsView(popup));
+      openPopup();
       document.addEventListener('keydown', onEscKeyDown);
     });
 
   };
 
+  #handleShowMoreButtonClick = (evt) => {
+    evt.preventDefault();
+    this.#movies.slice(this.#renderedMoviesCount, this.#renderedMoviesCount + MOVIES_COUNT_ROW).forEach((movie) => render(new FilmCardView(movie.filmInfo), this.#allMoviesContainer));
+    this.#renderedMoviesCount += MOVIES_COUNT_ROW;
+    if (this.#renderedMoviesCount > MOVIES_COUNT) {
+      this.#showMoreButton.element.remove();
+    }
+  };
+
   init = () => {
-    this.#movies = [...this.#moviesModel.movies]; // спред чтобы сделать новый массив и не сломать его
     render(this.#main, this.#mainContainer); // рендер .films
     render(this.#allMovies, this.#main.element);
     render(this.#topRated, this.#main.element);
     render(this.#mostCommented, this.#main.element);
-    render(new ShowMoreButtonView(), this.#allMovies.element);
-    const filmsDivElements = document.querySelectorAll('.films-list__container');
-    for (let i = 0; i < MOVIES_COUNT; i++) {
+
+    if (this.#movies.length > MOVIES_COUNT_ROW) {
+      render(this.#showMoreButton, this.#allMovies.element);
+      this.#showMoreButton.element.addEventListener('click', this.#handleShowMoreButtonClick);
+    }
+
+    for (let i = 0; i < Math.min(this.#movies.length, MOVIES_COUNT_ROW); i++) { // Выбирает минимальное значение из кол-ва фильмов и значения константы = 5
       const film = new FilmCardView(this.#movies[i].filmInfo);
-      render(film, filmsDivElements[0]);
+      render(film, this.#allMoviesContainer);
       this.#renderPopup(film, this.#movies[i]);
     }
-    for (let i = 0; i < MOVIES_COUNT_TOP; i++) {
+
+    for (let i = 0; i < Math.min(MOVIES_COUNT, MOVIES_COUNT_TOP); i++) {
       const film = new FilmCardView(this.#movies[i].filmInfo);
-      render(film, filmsDivElements[1]);
+      render(film, this.#topRatedContainer);
       this.#renderPopup(film, this.#movies[i]);
     }
-    for (let i = 0; i < MOVIES_COUNT_TOP; i++) {
+
+    for (let i = 0; i < Math.min(MOVIES_COUNT, MOVIES_COUNT_TOP); i++) {
       const film = new FilmCardView(this.#movies[i].filmInfo);
-      render(film, filmsDivElements[2]);
+      render(film, this.#mostCommentedContainer);
       this.#renderPopup(film, this.#movies[i]);
     }
   };
